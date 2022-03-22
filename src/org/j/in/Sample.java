ff77bf5762bd8d9bf1c4ef7dc59cbd39b2bf7fe0
jagdish0719 <jagdishtheking27@gmail.com>
2022-03-22 23:51:14 +0530

    siva done his code

--- src/org/j/in/Sample.java
+++ src/org/j/in/Sample.java
@@ -2,9 +2,14 @@ package org.j.in;
 
 public class Sample {
 	
+	private void p3() {
+System.out.println("siva write the coding");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("unique url");
+		System.out.println("p3 data");
 	}
 
 }
