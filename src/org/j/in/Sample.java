2d3bf49aa9536a2c0b4ce68b7f1f4b6446c73f4d
jagdish0719 <jagdishtheking27@gmail.com>
2022-03-22 22:41:03 +0530

    Linga added his code

--- src/org/j/in/Sample.java
+++ src/org/j/in/Sample.java
@@ -1,10 +1,15 @@
 package org.j.in;
 
 public class Sample {
-	
+
+	private void p2() {
+		System.out.println("linga write the coding");
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("unique url");
+		System.out.println("p2 datas");
 	}
 
 }
