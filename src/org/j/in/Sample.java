1b41254734cedfa55cbb64446630067777b405a6
jagdish0719 <jagdishtheking27@gmail.com>
2022-03-22 22:26:00 +0530

    Anu done her work

--- src/org/j/in/Sample.java
+++ src/org/j/in/Sample.java
@@ -1,10 +1,15 @@
 package org.j.in;
 
 public class Sample {
-	
+
+	private void p1() {
+		System.out.println("anu writing the code");
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("unique url");
+		System.out.println("data");
 	}
 
 }
